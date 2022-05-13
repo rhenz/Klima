16b76d2d9f35bc21abde02c928b49df829d89232
Rhenz <salvador.rhenz@gmail.com>
2022-05-13 13:51:48 +0800

    Project setup

---
+++ Klima/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  Klima
+//
+//  Created by John Salvador on 5/13/22.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
